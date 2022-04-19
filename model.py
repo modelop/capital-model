4ca51b18659350b34f3f8d7ce8e4dbb836912151
Jim Olsen <jim.olsen@modelop.com>
2022-04-19 08:09:59 -0600

    Adding dummy model and data files

---
+++ model.py
@@ -0,0 +1,16 @@
+
+
+# modelop.init
+def init():
+    print("Model Initialization")
+
+
+# modelop.score
+def action(data):
+    print("Do your scoring here")
+
+
+# modelop.metrics
+def metrics(data):
+    print("Do any built in metrics reporting here")
+
